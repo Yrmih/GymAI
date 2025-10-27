8604f7c94b4d47cfb4a0c718ba5d11515e0d1ceb
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-27 17:03:22 -0300

    feat: integrar Redux Provider e GluestackUIProvider ao App principal
    - Envolvido o App com o Provider do Redux para disponibilizar o estado global.
    - Adicionado o GluestackUIProvider com tema escuro para gerenciamento de UI.
    - Criado layout inicial simples com texto "Alô mundo" e StatusBar.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export default function App() {
   return (
-    
-    <GluestackUIProvider mode="dark">
-      <View style={styles.container}>
-      <Text>Alô mundo</Text>
-      <StatusBar style="auto" />
-    </View>
-    </GluestackUIProvider>
-  
+    <Provider store={store}>
+      <GluestackUIProvider mode="dark">
+        <View style={styles.container}>
+          <Text>Alô mundo</Text>
+          <StatusBar style="auto" />
+        </View>
+      </GluestackUIProvider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
