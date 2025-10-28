8fa9c29780e7d8fa6ce22afd01da078991d75597
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-28 16:54:30 -0300

    refactor - importando o mock para o componente de exercicios

--- app/(main)/Exercicies.tsx
+++ app/(main)/Exercicies.tsx
@@ -2,34 +2,7 @@ import React from "react";
 import { ScrollView, Text, View } from "react-native";
 import tw from "twin.macro";
 import ExerciseCard from "@/src/components/card/ExerciseCard";
-
-// Mock temporário de exercícios (futuramente vem da IA ou do Firebase)
-const exercises = [
-  {
-    id: 1,
-    name: "Supino Reto com Barra",
-    muscle: "Peitoral",
-    image: "https://static.strengthlevel.com/images/illustrations/bench-press-1000x1000.jpg",
-  },
-  {
-    id: 2,
-    name: "Agachamento Livre",
-    muscle: "Pernas / Glúteos",
-    image: "https://static.strengthlevel.com/images/illustrations/squat-1000x1000.jpg",
-  },
-  {
-    id: 3,
-    name: "Remada Curvada",
-    muscle: "Costas",
-    image: "https://static.strengthlevel.com/images/illustrations/bent-over-row-1000x1000.jpg",
-  },
-  {
-    id: 4,
-    name: "Desenvolvimento com Halteres",
-    muscle: "Ombros",
-    image: "https://static.strengthlevel.com/images/illustrations/dumbbell-shoulder-press-1000x1000.jpg",
-  },
-];
+import { exercisesMock } from "@/src/mock/exercisesMock";
 
 export default function Exercises() {
   return (
@@ -39,7 +12,7 @@ export default function Exercises() {
       </Text>
 
       <View>
-        {exercises.map((item) => (
+        {exercisesMock.map((item) => (
           <ExerciseCard
             key={item.id}
             name={item.name}
