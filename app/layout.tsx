7f0e1adb9e19b21db24444b65d503426ba8089de
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-29 12:51:15 -0300

    feat - definindo escopo do Layout raiz da pasta App

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { store } from "@/src/redux/store";
+import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
+
+export default function RootLayout(){
+  return (
+    
+  )
+}
