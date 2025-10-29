e4c757f814b44266c2f3782e0e0bb97b2d60f266
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-29 13:21:13 -0300

    Refactor: Ajusta layout de autenticação com Redux, Expo Router e animações Moti
    - Importa UseSelector do Redux para acessar o estado global.
    - Adiciona uso do hook useRouter do Expo Router para navegação.
    - Integra MotiView para animações no layout.
    - Utiliza o tailwind (twin.macro) para estilização do layout.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { UseSelector } from "react-redux";
+import { RootState } from "@/src/redux/store";
+import { useRouter } from "expo-router";
+import { MotiView } from "moti";
+import tw from "twin.macro";
