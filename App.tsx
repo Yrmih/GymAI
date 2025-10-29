f24964d2690562dd01f09bac4dc21a1c99323399
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-29 12:38:11 -0300

    refactor(app) - Adicionando Slot do Expo Router para configurar roteamento das telas do app

--- App.tsx
+++ App.tsx
@@ -1,31 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
 import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
 import { Provider } from "react-redux";
-import { store } from "./src/redux/store";
+import { store } from "@/src/redux/store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <Provider store={store}>
       <GluestackUIProvider mode="dark">
         <SafeAreaProvider>
-          <SafeAreaView style={styles.container}>
-            <Text>Alô mundo</Text>
-            <StatusBar style="auto" />
-          </SafeAreaView>
+          <Slot />
+          <StatusBar style="light" />
         </SafeAreaProvider>
       </GluestackUIProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
