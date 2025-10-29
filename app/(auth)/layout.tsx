3efb2aa077516e35e8e195c6dcd2dc05342eed95
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-29 14:39:58 -0300

    feat - Adiciona lógica de redirecionamento para a tela de login no MainLayout
    - Usa `useEffect` para monitorar o estado de login do usuário (`usuarioLogado`).
    - Se o usuário não estiver logado, redireciona automaticamente para a página de login utilizando `router.replace`.
    - Implementa animação de entrada e saída com `MotiView` para transições suaves.
    - Aplica estilização com `twin.macro` para o fundo e layout flexível.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,23 @@
+// app/(auth)
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
-import { useSelector, UseSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/src/redux/store";
 import { useRouter } from "expo-router";
 import { MotiView } from "moti";
 import tw from "twin.macro";
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const usuarioLogado = useSelector((state: RootState) => state.usuario.logado);
 
   useEffect(() => {
     if (usuarioLogado) {
+      
       router.replace("/(main)/index");
     }
   }, [usuarioLogado]);
+
   return (
     <MotiView
       from={{ opacity: 0 }}
