fd974ce7fbdc22b985ddf7174e3ee7da24e0a945
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-11-08 11:23:43 -0300

    chore(splashscreen) - corrigindo bug do video que esta em loop

--- app/SplashScreen.tsx
+++ app/SplashScreen.tsx
@@ -13,48 +13,32 @@ export default function SplashScreen({ onFinish }: SplashScreenProps) {
   const router = useRouter();
   const usuarioLogado = useSelector((state: RootState) => state.usuario.logado);
 
-  // 2. Criamos uma "referência" para o componente de Vídeo
   const videoRef = useRef<Video>(null);
 
   const handlePlaybackStatus = async (status: AVPlaybackStatus) => {
-    // Se não estiver carregado OU se já terminamos, não faça nada.
     if (!status.isLoaded) return;
 
     if (status.didJustFinish) {
-      
-      await videoRef.current?.pauseAsync(); 
-      
+      await videoRef.current?.pauseAsync();
+
       if (usuarioLogado) {
         onFinish();
       } else {
         router.replace("/(auth)/Login");
+        onFinish();
       }
     }
   };
 
-  // O seu useEffect (que está CORRETO) vai "ouvir" o hasFinished
-  // e navegar assim que ele virar true.
-  useEffect(() => {
-    if (!hasFinished) return;
-
-    if (usuarioLogado) {
-      onFinish(); 
-    } else {
-      router.replace("/(auth)/Login"); 
-      onFinish(); 
-    }
-  }, [hasFinished]); 
-
   return (
     <Video
-      ref={videoRef} // 4. Passamos o ref para o componente
+      ref={videoRef}
       source={require("../assets/video/AI_Bodybuilding_Cinematic_Splash_Screen.mp4")}
       style={StyleSheet.absoluteFill}
       isMuted
-      shouldPlay={!hasFinished} // Isso ainda ajuda
       resizeMode={ResizeMode.COVER}
-      onPlaybackStatusUpdate={onPlaybackStatusUpdate}
-      isLooping={false} // Mantemos isso por segurança
+      onPlaybackStatusUpdate={handlePlaybackStatus}
+      isLooping={false}
     />
   );
 }
