c24278f9a2016cc859cf8b34f3dd417e74981179
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-11-13 17:12:38 -0300

    feat(redux): atualiza usuarioSlice com novos campos - Adiciona `frequenciaSemanal`, `gruposPrioritarios` e `lesoes` no reducer `updateUserBody` - Adiciona manipulação desses campos na ação de `logout`

--- src/redux/usuarioSlice.ts
+++ src/redux/usuarioSlice.ts
@@ -21,17 +21,33 @@ export const usuarioSlice = createSlice({
       state.peso = undefined;
       state.biotipo = undefined;
       state.tempoTreino = undefined;
+      state.frequenciaSemanal = undefined;
+      state.gruposPrioritarios = undefined;
+      state.lesoes = undefined;
     },
     updateUserBody: (state, action: PayloadAction<{
       altura: number;
       peso: number;
       biotipo: string;
       tempoTreino: string;
+      // novos campos
+      frequenciaSemanal?: string;
+      gruposPrioritarios?: string;
+      lesoes?: string;
     }>) => {
       state.altura = action.payload.altura;
       state.peso = action.payload.peso;
       state.biotipo = action.payload.biotipo;
       state.tempoTreino = action.payload.tempoTreino;
+
+      if (action.payload.frequenciaSemanal)
+        state.frequenciaSemanal = action.payload.frequenciaSemanal;
+
+      if (action.payload.gruposPrioritarios)
+        state.gruposPrioritarios = action.payload.gruposPrioritarios;
+
+      if (action.payload.lesoes)
+        state.lesoes = action.payload.lesoes;
     }
   },
 });
