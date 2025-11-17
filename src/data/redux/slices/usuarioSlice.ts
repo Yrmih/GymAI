e4717364fe92791273ab7d9565dcb6365e344f39
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-11-16 22:49:06 -0300

    refactor mudanças na organização das pastas redux

---
+++ src/data/redux/slices/usuarioSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Usuario } from "@/src/types/usuario";
+import { PerfilUsuario } from "@/src/types/perfil";
+
+interface UsuarioState {
+  usuario: Usuario & Partial<PerfilUsuario>;
+}
+
+const initialState: UsuarioState = {
+  usuario: {
+    nome: "",
+    logado: false,
+    altura: undefined,
+    peso: undefined,
+    biotipo: undefined,
+    tempoTreino: undefined,
+    email: "",
+    foto: "",
+    token: "",
+  },
+};
+
+const usuarioSlice = createSlice({
+  name: "usuario",
+  initialState,
+  reducers: {
+    setUsuario: (state, action: PayloadAction<Partial<Usuario & PerfilUsuario>>) => {
+      state.usuario = { ...state.usuario, ...action.payload, logado: true };
+    },
+    logoutUsuario: (state) => {
+      state.usuario = {
+        nome: "",
+        logado: false,
+        altura: undefined,
+        peso: undefined,
+        biotipo: undefined,
+        tempoTreino: undefined,
+        email: "",
+        foto: "",
+        token: "",
+      };
+    },
+  },
+});
+
+export const { setUsuario, logoutUsuario } = usuarioSlice.actions;
+export default usuarioSlice.reducer;
---
+++ src/data/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usuarioReducer from './usuarioSlice';
+
+export const store = configureStore({
+  reducer: {
+    usuario: usuarioReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
